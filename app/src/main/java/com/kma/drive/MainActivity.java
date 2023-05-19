e2ada861ab122beb6a8be4e6cb55400000c4db9d
admin <ducank961@gmail.com>
2023-05-19 10:31:20 +0700

    Update template

--- app/src/main/java/com/kma/drive/MainActivity.java
+++ app/src/main/java/com/kma/drive/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ListView;
 
@@ -22,5 +23,18 @@ public class MainActivity extends AppCompatActivity {
         DataAdapter adapter = new DataAdapter(this, items);
         listView.setAdapter(adapter);
 
+        hideNavigationBar();
+    }
+
+    private void hideNavigationBar() {
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+
+        );
     }
 }
