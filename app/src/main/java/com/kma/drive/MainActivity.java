30168422a42abe068fa08cba1dcb74eb771c3f97
admin <ducank961@gmail.com>
2023-05-30 23:54:24 +0700

    use RecycleView

--- app/src/main/java/com/kma/drive/MainActivity.java
+++ app/src/main/java/com/kma/drive/MainActivity.java
@@ -1,40 +1,52 @@
 package com.kma.drive;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ArrayList<SanPham> listSanPham;
+    SanPhamAdapter sanPhamAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageButton imageButton_menu = (ImageButton) findViewById(R.id.id_menu);
+        // set color for image
+        ImageButton imageButton_menu = findViewById(R.id.id_menu);
         imageButton_menu.setColorFilter(Color.BLACK);
 
-        String[] items = {"Folder 1", "Folder 2", "Folder 3"};
-        ListView listView = (ListView) findViewById(R.id.list_view);
-
-        DataAdapter adapter = new DataAdapter(this, items);
-        listView.setAdapter(adapter);
-
+        // Recycle view
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        listSanPham = new ArrayList<>();
+        listSanPham.add(new SanPham("Folder 1", R.drawable.folder_shared));
+        listSanPham.add(new SanPham("Folder 2", R.drawable.ic_folder));
+        listSanPham.add(new SanPham("Folder 3", R.drawable.folder_shared));
+        listSanPham.add(new SanPham("Folder 3", R.drawable.folder_shared));
+        listSanPham.add(new SanPham("Folder 3", R.drawable.folder_shared));
+        listSanPham.add(new SanPham("Folder 3", R.drawable.folder_shared));
+        sanPhamAdapter = new SanPhamAdapter(getApplicationContext(), listSanPham);
+        recyclerView.setAdapter(sanPhamAdapter);
         hideNavigationBar();
     }
 
     private void hideNavigationBar() {
         this.getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                        View.SYSTEM_UI_FLAG_FULLSCREEN |
-                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
-                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         );
     }
 }
