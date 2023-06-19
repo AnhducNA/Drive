1ae746c2bb43b44a9dbb81d181727ea8d34fa96f
admin <ducank961@gmail.com>
2023-06-20 00:16:53 +0700

    done

--- app/src/main/java/com/kma/drive/view/fragment/BaseAbstractFragment.java
+++ app/src/main/java/com/kma/drive/view/fragment/BaseAbstractFragment.java
@@ -9,11 +9,18 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.kma.drive.R;
+import com.kma.drive.adapter.SanPhamAdapter;
 import com.kma.drive.callback.FragmentCallback;
+import com.kma.drive.model.SanPham;
 import com.kma.drive.util.HttpRequestHelper;
 
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 
 public abstract class BaseAbstractFragment extends Fragment{
     protected WeakReference<Context> mContext;
@@ -31,8 +38,26 @@ public abstract class BaseAbstractFragment extends Fragment{
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(getLayout(), container, false);
-        return v;
+        View rootView = inflater.inflate(getLayout(), container, false);
+        // 1. get a reference to recyclerView
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.home_recyclerView);
+        // 2. set layoutManger
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        // this is data from recycler view
+        ArrayList<SanPham> listSanPham  = new ArrayList<>();
+        listSanPham.add( new SanPham("Folder1", R.drawable.ic_folder));
+        listSanPham.add( new SanPham("Folder1", R.drawable.ic_folder));
+        listSanPham.add( new SanPham("Folder1", R.drawable.folder_shared));
+        listSanPham.add( new SanPham("Folder1", R.drawable.folder_shared));
+
+        // 3. create an adapter
+        SanPhamAdapter mAdapter = new SanPhamAdapter(this.getContext(), listSanPham);
+        // 4. set adapter
+        recyclerView.setAdapter(mAdapter);
+        // 5. set item animator to DefaultAnimator
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        return rootView;
     }
 
     @Override
